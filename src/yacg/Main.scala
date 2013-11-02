16e67365936dcf8ef0a3491b98b87675cf2d0fa3
rob <rob@localhost.localdomain>
2013-11-02 17:30:51 +0100

    render a 4 pieced-heightmap

--- src/yacg/Main.scala
+++ src/yacg/Main.scala
@@ -32,8 +32,6 @@ class Main extends SimpleApplication {
  
     private var mat_terrain: Material = _
     private var terrain: TerrainGrid = _
-    private var grassScale: Float = 64
-    private var dirtScale: Float = 16
     
 
 
@@ -42,7 +40,7 @@ class Main extends SimpleApplication {
     assetManager.registerLocator("/home/rob/development/eclipse/workspace/yacg_2/assets/", classOf[FileLocator])
 
     //@TODO terrain collision detection for walking on it
-    flyCam.setMoveSpeed(100f)
+    flyCam.setMoveSpeed(200f)
     
     initMaterial
     initTerrain
@@ -52,7 +50,7 @@ class Main extends SimpleApplication {
 
     
   def initCamera: Unit = {
-    this.getCamera().setLocation(new Vector3f(0, 200, 0))
+    this.getCamera().setLocation(new Vector3f(0, 500, 0))
     this.getCamera().lookAt(new Vector3f(0,0,0), Vector3f.UNIT_Y)
     this.viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f))
   }
@@ -61,21 +59,9 @@ class Main extends SimpleApplication {
   def initMaterial: Unit = {
     //@TODO explain, where this j3md is and what it does
     this.mat_terrain = new Material(this.assetManager, "Common/MatDefs/Terrain/HeightBasedTerrain.j3md") //
-
-    /*
-    val grass = this.assetManager.loadTexture("Textures/grass.jpg")
-    grass.setWrap(WrapMode.Repeat)
-    this.mat_terrain.setTexture("region1ColorMap", grass)
-    this.mat_terrain.setVector3("region1", new Vector3f(-10, 0, this.grassScale))
-
-    val dirt = this.assetManager.loadTexture("Textures/dirt.jpg")
-    dirt.setWrap(WrapMode.Repeat)
-    this.mat_terrain.setTexture("region2ColorMap", dirt)
-    this.mat_terrain.setVector3("region2", new Vector3f(0, 900, this.dirtScale))
-     */
     
     //@TODO: use a intentionally designed texture for the whole terrain
-    val rock = this.assetManager.loadTexture("Textures/stone.jpg")
+    val rock = this.assetManager.loadTexture("Textures/misc/stone.jpg")
     rock.setWrap(WrapMode.Repeat)
 
     this.mat_terrain.setTexture("slopeColorMap", rock)
@@ -85,27 +71,33 @@ class Main extends SimpleApplication {
 
   def initLight: Unit = {
     val light = new DirectionalLight()
-    light.setDirection((new Vector3f(-0.5f, -1f, -0.5f)).normalize())
-    rootNode.addLight(light);
+    light.setDirection((new Vector3f(-1.0f, 1f, 1.0f)).normalize())
+    //rootNode.addLight(light);
   }
 
 
   def initTerrain: Unit = {
     //@TODO explain, why not every patch looks similar, although it's always the same png
-    this.terrain = new TerrainGrid("terrain", 65, 257, new ImageTileLoader(assetManager, 
+    this.terrain = new TerrainGrid("terrain", 512+1, 1024+1, new ImageTileLoader(assetManager, 
       new Namer() {
-        def getName(x: Int, y: Int): String = "Textures/heightmap.png"
+        def getName(x: Int, y: Int): String = {
         	//@TODO don't always the same heightfield 
             //return "Interface/Scenes/TerrainMountains/terrain_" + x + "_" + y + ".png";
+        	val x_patch = Math.abs(x) % 2;
+        	val y_patch = Math.abs(y) % 2;
+        	System.out.println("" + x + ", " +y + " -> " + x_patch + ", " + y_patch)
+        	"Textures/heightmap/heightmap_" + x_patch + "_" + y_patch + ".png"
+        }
     }));
 
+    
     this.terrain.setMaterial(mat_terrain)
     this.terrain.setLocalTranslation(0, 0, 0)
-    this.terrain.setLocalScale(3f, 1.5f, 3f)
+    //this.terrain.setLocalScale(3f, 1.5f, 3f)
     this.rootNode.attachChild(this.terrain)
 
     val control = new TerrainGridLodControl(this.terrain, getCamera())
-    control.setLodCalculator( new DistanceLodCalculator(64, 2.7f) )  // patch size, and a multiplier
+    control.setLodCalculator( new DistanceLodCalculator(512, 2.7f) )  // patch size, and a multiplier
     this.terrain.addControl(control)
   }
 
