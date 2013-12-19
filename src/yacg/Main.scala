80c68f0fab3d34763672679be49a8e1accaa5c9b
rob <rob@localhost.localdomain>
2013-12-19 21:43:51 +0100

    -yellow box falling from the sky and moving in x direction

--- src/yacg/Main.scala
+++ src/yacg/Main.scala
@@ -1,9 +1,8 @@
 package yacg
 
-
 import com.jme3.app.SimpleApplication
 import com.jme3.material.Material
-import com.jme3.math.{Vector3f,ColorRGBA}
+import com.jme3.math.{ Vector3f, ColorRGBA }
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
 import com.jme3.terrain.geomipmap._
@@ -27,13 +26,13 @@ import com.jme3.bullet.control.CharacterControl
 import com.jme3.bullet.collision.shapes.HeightfieldCollisionShape
 import com.jme3.terrain.heightmap.AbstractHeightMap
 import com.jme3.terrain.heightmap.ImageBasedHeightMap
-
-
+import com.jme3.math.FastMath
+import com.jme3.scene.Mesh
 
 object Main {
-  def main(args:Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-    import java.util.logging.{Logger,Level}
+    import java.util.logging.{ Logger, Level }
     Logger.getLogger("").setLevel(Level.WARNING);
 
     val app = new Main
@@ -41,74 +40,73 @@ object Main {
   }
 }
 
-
 class Main extends SimpleApplication with ActionListener {
- 
-    private var mat_terrain: Material = _
-    private var terrain: TerrainGrid = _
-//    private var terrain: TerrainQuad = _
-    private var bulletAppState: BulletAppState = _
-    private var left: Boolean = false;
-    private var right: Boolean = false;
-    private var up: Boolean = false;
-    private var down: Boolean = false;
-    private var player: CharacterControl = _
-    var camDir: Vector3f = new Vector3f()
-  	var camLeft: Vector3f = new Vector3f()    
-  	var walkDirection: Vector3f = new Vector3f()    
 
+  private var mat_terrain: Material = _
+  private var terrain: TerrainGrid = _
+  //    private var terrain: TerrainQuad = _
+  private var bulletAppState: BulletAppState = _
+  private var left: Boolean = false;
+  private var right: Boolean = false;
+  private var up: Boolean = false;
+  private var down: Boolean = false;
+  private var player: CharacterControl = _
+  var camDir: Vector3f = new Vector3f()
+  var camLeft: Vector3f = new Vector3f()
+  var walkDirection: Vector3f = new Vector3f()
+  private var boxie: Geometry = _
+  private var boxie_location = new Vector3f(0, 450, 0)
 
   override def simpleInitApp: Unit = {
     assetManager.registerLocator(System.getProperty("user.dir") + "/assets/", classOf[FileLocator])
 
     //@TODO terrain collision detection for walking on it
     //flyCam.setMoveSpeed(500f)
-    
+
     initMaterial
     initTerrain
     initCamera
     //initLight
     setUpKeys
+    createCube
   }
 
-    
   def initCamera: Unit = {
     this.getCamera().setLocation(new Vector3f(0, 10, 0))
-    this.getCamera().lookAt(new Vector3f(0,0,0), Vector3f.UNIT_Y)
+    this.getCamera().lookAt(boxie_location, Vector3f.UNIT_Y)
     this.viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f))
     this.getCamera().setFrustumFar(10000.0f)
     //flyCam.setEnabled(false)
 
-	bulletAppState = new BulletAppState()
-	stateManager.attach(bulletAppState)
-	System.out.println("attach bulletappstate to statemgr")
+    bulletAppState = new BulletAppState()
+    stateManager.attach(bulletAppState)
+    System.out.println("attach bulletappstate to statemgr")
 
-	val capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1)
-	
-	//val capsuleShape = new BoxCollisionShape()
+    val capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1)
+
+    //val capsuleShape = new BoxCollisionShape()
     //player = new BetterCharacterControl(1.0f,1.0f,1.0f) //capsuleShape, 0.05f)
     //player.setGravity(new Vector3f(0,-10,0))
     player = new CharacterControl(capsuleShape, 0.05f)
     player.setPhysicsSpace(bulletAppState.getPhysicsSpace())
     //player.
-    
-	player.setJumpSpeed(20)
-	player.setFallSpeed(30)
-	player.setGravity(30)
-	player.setPhysicsLocation(new Vector3f(0,100,100))
-	 
-	System.out.println("bulletAppState phyicspace.add terrain")
-	bulletAppState.getPhysicsSpace().add(terrain);
-	System.out.println("bulletAppState ps.add player")
-	bulletAppState.getPhysicsSpace().add(player);   
+
+    player.setJumpSpeed(20)
+    player.setFallSpeed(30)
+    player.setGravity(30)
+    player.setPhysicsLocation(new Vector3f(0, 100, 100))
+
+    System.out.println("bulletAppState phyicspace.add terrain")
+    bulletAppState.getPhysicsSpace().add(terrain);
+    System.out.println("bulletAppState ps.add player")
+    bulletAppState.getPhysicsSpace().add(player);
 
   }
 
-  
   def initMaterial: Unit = {
     //@TODO explain, where this j3md is and what it does
     this.mat_terrain = new Material(this.assetManager, "Common/MatDefs/Terrain/HeightBasedTerrain.j3md") //
-    
+
     //@TODO: use a intentionally designed texture for the whole terrain
     val rock = this.assetManager.loadTexture("Textures/misc/stone.jpg")
     rock.setWrap(WrapMode.Repeat)
@@ -125,47 +123,45 @@ class Main extends SimpleApplication with ActionListener {
     //rootNode.addLight(light);
   }
 
-
   def initTerrain: Unit = {
 
-    
-    this.terrain = new TerrainGrid("terrain", 512+1, 1024+1, new ImageTileLoader(assetManager, 
+    this.terrain = new TerrainGrid("terrain", 512 + 1, 1024 + 1, new ImageTileLoader(assetManager,
       new Namer() {
         def getName(x: Int, y: Int): String = {
-        	//@TODO don't always the same heightfield 
-            //return "Interface/Scenes/TerrainMountains/terrain_" + x + "_" + y + ".png";
-        	val x_patch = Math.abs(x) % 2;
-        	val y_patch = Math.abs(y) % 2;
-        	// System.out.println("" + x + ", " +y + " -> " + x_patch + ", " + y_patch)
-        	"Textures/heightmap/heightmap_" + x_patch + "_" + y_patch + ".png"
+          //@TODO don't always the same heightfield 
+          //return "Interface/Scenes/TerrainMountains/terrain_" + x + "_" + y + ".png";
+          val x_patch = Math.abs(x) % 2;
+          val y_patch = Math.abs(y) % 2;
+          // System.out.println("" + x + ", " +y + " -> " + x_patch + ", " + y_patch)
+          "Textures/heightmap/heightmap_" + x_patch + "_" + y_patch + ".png"
         }
-    }));
+      }));
     val control = new TerrainGridLodControl(this.terrain, getCamera())
-    control.setLodCalculator( new DistanceLodCalculator(512, 2.7f) )  // patch size, and a multiplier
+    control.setLodCalculator(new DistanceLodCalculator(512, 2.7f)) // patch size, and a multiplier
 
     terrain.addListener(new TerrainGridListener() {
 
-                def gridMoved(newCenter: Vector3f) {
-                }
+      def gridMoved(newCenter: Vector3f) {
+      }
 
-                def tileAttached(cell: Vector3f, quad: TerrainQuad) {
-                    while(quad.getControl(classOf[RigidBodyControl])!=null){
-                        quad.removeControl(classOf[RigidBodyControl]);
-                    }
-                    quad.addControl(new RigidBodyControl(new HeightfieldCollisionShape(quad.getHeightMap(), terrain.getLocalScale()), 0));
-                    bulletAppState.getPhysicsSpace().add(quad);
-                }
+      def tileAttached(cell: Vector3f, quad: TerrainQuad) {
+        while (quad.getControl(classOf[RigidBodyControl]) != null) {
+          quad.removeControl(classOf[RigidBodyControl]);
+        }
+        quad.addControl(new RigidBodyControl(new HeightfieldCollisionShape(quad.getHeightMap(), terrain.getLocalScale()), 0));
+        bulletAppState.getPhysicsSpace().add(quad);
+      }
 
-                def tileDetached(cell: Vector3f, quad: TerrainQuad) {
-                    if (quad.getControl(classOf[RigidBodyControl]) != null) {
-                        bulletAppState.getPhysicsSpace().remove(quad);
-                        quad.removeControl(classOf[RigidBodyControl]);
-                    }
-                }
+      def tileDetached(cell: Vector3f, quad: TerrainQuad) {
+        if (quad.getControl(classOf[RigidBodyControl]) != null) {
+          bulletAppState.getPhysicsSpace().remove(quad);
+          quad.removeControl(classOf[RigidBodyControl]);
+        }
+      }
 
-            });    
-    
-/*
+    });
+
+    /*
     var heightMapImage = assetManager.loadTexture("Textures/heightmap/heightmap_0_0.png")
     var heightmap = new ImageBasedHeightMap(heightMapImage.getImage());
     heightmap.load();    
@@ -173,47 +169,47 @@ class Main extends SimpleApplication with ActionListener {
     val control = new TerrainLodControl(this.terrain, getCamera())
     control.setLodCalculator( new DistanceLodCalculator(512, 2.7f) )  // patch size, and a multiplier
 * */
-    
+
     this.terrain.setMaterial(mat_terrain)
     this.terrain.setLocalTranslation(0, 0, 0)
     //this.terrain.setLocalScale(10f, 3f, 10f)
-	System.out.println("add terrain to rootnode")
+    System.out.println("add terrain to rootnode")
     this.rootNode.attachChild(this.terrain)
 
-
-	//System.out.println("add lodctrlr to terrain")
+    //System.out.println("add lodctrlr to terrain")
     this.terrain.addControl(control)
 
-	System.out.println("terrain.addctrl riidbocy")
+    System.out.println("terrain.addctrl riidbocy")
     terrain.addControl(new RigidBodyControl(0))
-    
-	//val terrainCollisionShape = new HeightfieldCollisionShape(terrain.getHeightMap(), terrain.getLocalScale())
-	//val terrain_phy = new RigidBodyControl(terrainCollisionShape, 0f)
-	//this.terrain.addControl(terrain_phy);
+
+    //val terrainCollisionShape = new HeightfieldCollisionShape(terrain.getHeightMap(), terrain.getLocalScale())
+    //val terrain_phy = new RigidBodyControl(terrainCollisionShape, 0f)
+    //this.terrain.addControl(terrain_phy);
 
   }
 
+  def setUpKeys: Unit = {
+    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+    inputManager.addListener(this, "Left");
+    inputManager.addListener(this, "Right");
+    inputManager.addListener(this, "Up");
+    inputManager.addListener(this, "Down");
+    inputManager.addListener(this, "Jump");
+  }
 
-	def  setUpKeys: Unit = {
-	    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
-	    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
-	    inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
-	    inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
-	    inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
-	    inputManager.addListener(this, "Left");
-	    inputManager.addListener(this, "Right");
-	    inputManager.addListener(this, "Up");
-	    inputManager.addListener(this, "Down");
-	    inputManager.addListener(this, "Jump");
-    }
- 
-  /** These are our custom actions triggered by key presses.
-   * We do not walk yet, we just keep track of the direction the user pressed. */
+  /**
+   * These are our custom actions triggered by key presses.
+   * We do not walk yet, we just keep track of the direction the user pressed.
+   */
   def onAction(binding: String, isPressed: Boolean, tpf: Float) {
     if (binding.equals("Left")) {
       left = isPressed;
     } else if (binding.equals("Right")) {
-      right= isPressed;
+      right = isPressed;
     } else if (binding.equals("Up")) {
       up = isPressed;
     } else if (binding.equals("Down")) {
@@ -222,7 +218,7 @@ class Main extends SimpleApplication with ActionListener {
       if (isPressed) { player.jump(); }
     }
   }
- 
+
   /**
    * This is the main event loop--walking happens here.
    * We check in which direction the player is walking by interpreting
@@ -231,27 +227,50 @@ class Main extends SimpleApplication with ActionListener {
    * We also make sure here that the camera moves with player.
    */
   //@Override
-    override def simpleUpdate(tpf: Float): Unit = {
-        camDir.set(cam.getDirection()).multLocal(2.6f);
-        camLeft.set(cam.getLeft()).multLocal(0.4f);
-        walkDirection.set(0, 0, 0);
-        if (left) {
-            walkDirection.addLocal(camLeft);
-        }
-        if (right) {
-            walkDirection.addLocal(camLeft.negate());
-        }
-        if (up) {
-            walkDirection.addLocal(camDir);
-        }
-        if (down) {
-            walkDirection.addLocal(camDir.negate());
-        }
-        player.setWalkDirection(walkDirection);
-        cam.setLocation(player.getPhysicsLocation());
-    }  
-  
-  
+  override def simpleUpdate(tpf: Float): Unit = {
+    camDir.set(cam.getDirection()).multLocal(2.6f);
+    camLeft.set(cam.getLeft()).multLocal(0.4f);
+    walkDirection.set(0, 0, 0);
+    if (left) {
+      walkDirection.addLocal(camLeft);
+    }
+    if (right) {
+      walkDirection.addLocal(camLeft.negate());
+    }
+    if (up) {
+      walkDirection.addLocal(camDir);
+    }
+    if (down) {
+      walkDirection.addLocal(camDir.negate());
+    }
+    player.setWalkDirection(walkDirection);
+    cam.setLocation(player.getPhysicsLocation());
+
+    doCubeStuff(tpf);
+  }
+
+  def doCubeStuff(tpf: Float) {
+    boxie.move(10*tpf, 0, 0)
+  }
+
+  def createCube() {
+    var b = new Box(Vector3f.ZERO, 4, 4, 4);
+    boxie = createGeometryFromMesh(b, "box", boxie_location, ColorRGBA.Yellow)
+    boxie.addControl(new RigidBodyControl(1.0f))
+    bulletAppState.getPhysicsSpace().add(boxie)
+    rootNode.attachChild(boxie)
+  }
+
+  def createGeometryFromMesh(mesh: Mesh, name: String, loc: Vector3f, color: ColorRGBA): Geometry =
+    {
+      var geom = new Geometry(name, mesh)
+      var mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
+      mat.setColor("Color", color)
+      geom.setMaterial(mat)
+      geom.setLocalTranslation(loc)
+      geom;
+    }
+
 }
 
 
